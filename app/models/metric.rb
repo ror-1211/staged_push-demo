3d55f319ed6e5c984b17211d629587820417224e
ror-1211 <timothyhughest185@gmail.com>
2021-12-25 11:05:33 +0100

    Add Metric model

---
+++ app/models/metric.rb
@@ -0,0 +1,5 @@
+class Metric < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+	validates :value, presence: true, numericality:
+    { only_integer: true, greater_than_or_equal_to: 0 }
+end
---
+++ db/migrate/20211225095555_create_metrics.rb
@@ -0,0 +1,10 @@
+class CreateMetrics < ActiveRecord::Migration[7.0]
+  def change
+    create_table :metrics do |t|
+      t.string :name, null: false, index: { unique: true }
+      t.integer :value, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
